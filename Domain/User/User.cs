2756ba0c88783673b85789e0c218e9aaa0cad42c
hannahhaken <hannahhaken@gmail.com>
2025-01-10 16:29:52 +0100

    Defines domain behaviour for User: methods for updating fields and tracking times of changes

--- Domain/User/User.cs
+++ Domain/User/User.cs
@@ -2,14 +2,14 @@ namespace Domain.User;
 
 public class User
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; private set; }
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
     public required string Email { get; set; }
     public required string Password { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
-    public DateTime? LastLoggedInAt { get; set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+    public DateTime? LastLoggedInAt { get; private set; }
 
     public User(string firstName, string lastName, string email, string password)
     {
@@ -20,5 +20,34 @@ public class User
         Password = password;
         CreatedAt = DateTime.UtcNow;
         UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void UpdateFirstName(string newFirstName)
+    {
+        FirstName = newFirstName;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void UpdateLastName(string newLastName)
+    {
+        FirstName = newLastName;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void UpdateEmail(string newEmail)
+    {
+        Email = newEmail;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void UpdatePassword(string newPassword)
+    {
+        Password = newPassword;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void UpdateLastLoggedInAt()
+    {
+        LastLoggedInAt = DateTime.UtcNow;
     }
 }
