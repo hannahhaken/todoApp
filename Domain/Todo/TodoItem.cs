3e821cea4bad6987c4ecc98e46aa5b210d18edd6
hannahhaken <hannahhaken@gmail.com>
2025-01-10 14:38:26 +0100

    Adds constructor-based initialisation for TodoItem and user

--- Domain/Todo/TodoItem.cs
+++ Domain/Todo/TodoItem.cs
@@ -3,22 +3,25 @@ namespace Domain.Todo;
 public class TodoItem
 {
     public Guid Id { get; set; }
-
     public required string Title { get; set; }
-
     public string? Description { get; set; }
-
     public TodoStatus Status { get; set; }
-
     public DateTime CreatedAt { get; set; }
-
     public DateTime UpdatedAt { get; set; }
-
     public DateTime? DueAt { get; set; }
-
     public DateTime? CompletedAt { get; set; }
-
     public DateTime? DeletedAt { get; set; }
-
     public Guid UserId { get; set; }
+
+    public TodoItem(Guid userId, string title, string? description = null, DateTime? dueAt = null)
+    {
+        Id = Guid.NewGuid();
+        UserId = userId;
+        Title = title;
+        Description = description;
+        Status = TodoStatus.Due;
+        DueAt = dueAt;
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
 }
--- Domain/User/User.cs
+++ Domain/User/User.cs
@@ -3,18 +3,22 @@ namespace Domain.User;
 public class User
 {
     public Guid Id { get; set; }
-
     public required string FirstName { get; set; }
-
     public required string LastName { get; set; }
-
     public required string Email { get; set; }
-
     public required string Password { get; set; }
-
     public DateTime CreatedAt { get; set; }
-
     public DateTime UpdatedAt { get; set; }
-
     public DateTime? LastLoggedInAt { get; set; }
+
+    public User(string firstName, string lastName, string email, string password)
+    {
+        Id = Guid.NewGuid();
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
 }
